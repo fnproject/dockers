8771e5391944527cc4ff91d4cf0e1ad8434de6f5
Travis Reeder <treeder@gmail.com>
2017-06-05 17:01:50 -0700

    Updated python build script

--- python/build.rb
+++ python/build.rb
@@ -1,5 +1,3 @@
-
-require 'open3'
 require_relative '../utils/builder'
 
 name = "funcy/python"
@@ -14,13 +12,13 @@ p v
 # returns: Python 2.7.11
 v = v.split(' ')[1]
 p v
-vtag(name, tag, v)
+new_tags = vtag(name, tag, v)
 
 Dir.chdir 'dev'
 tag = "2-dev"
 build("#{name}:#{tag}")
 p v
-vtag(name, tag, v, true)
+new_tags += vtag(name, tag, v, true)
 
 # now 3
 Dir.chdir '../../python3'
@@ -32,10 +30,12 @@ p v
 # returns: Python 2.7.11
 v = v.split(' ')[1]
 p v
-vtag(name, tag, v, false)
+new_tags += vtag(name, tag, v, false)
 
 Dir.chdir 'dev'
 tag = "dev"
 build("#{name}:#{tag}")
 p v
-vtag(name, tag, v, true)
+new_tags += vtag(name, tag, v, true)
+
+push_all(name, new_tags)
